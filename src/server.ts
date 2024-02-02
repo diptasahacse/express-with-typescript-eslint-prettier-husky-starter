c3d2fb3f91839ae3ab85154b01da795983348742
diptasahacse <diptasaha.lpu.cse@gmail.com>
2024-02-02 20:13:20 +0600

    project setup

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+const main = async () => {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("Database is connected");
+    app.listen(config.port, () => {
+      console.log(`application listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+main();
